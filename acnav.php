0d99fbeca58f278a007c45c1fc76c8b05e42aa2e
bashirmurtaza93 <bashirmurtaza93@gmail.com>
2017-02-17 16:51:27 -0500

    Ability to create campaign. Dashboard now has live data

---
+++ acnav.php
@@ -0,0 +1,13 @@
+<?php echo '<div class="col-xs-12" style="border-top: 1px solid rgb(210,215,220); border-bottom:1px solid rgb(210,215,220); height:66px;">
+       <a href="/dashboard.php"><img src="/assets/images/at-logo-black.png" style="margin-top:-8px;"></a>
+
+    </div>
+<!-- Content where the discover, communicatie, order management would be -->
+<div class="mininav" style="margin-top:65px" >
+    <p class="nav2"> <a href="/acdiscover.php" class="other-nav discover-nav"> DISCOVER </a> </p>
+      <p class="nav2"> <a href="" class="other-nav create-nav"> CREATE </a> </p>
+        <p class="nav2"> <a href="" class="other-nav price-nav">  PRICE CAMPAIGN </a></p>
+          <p class="nav2"> <a href="/dashboard.php" class="other-nav campaign-nav"> YOUR CAMPAIGNS </a>  </p>
+</div>';
+
+?>
