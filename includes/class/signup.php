390c529662c2cf1986a0e8bddf69f0cae14a87af
bashirmurtaza93 <bashirmurtaza93@gmail.com>
2017-02-24 16:47:48 -0500

    fixed some confirmation stuff

--- includes/class/signup.php
+++ includes/class/signup.php
@@ -57,11 +57,11 @@ class signUp extends favorite{
 
 
 public function sendConfirmationEmail($email,$confirmationkey){
-$headers = "From: support@project.social \r\n";
+$headers = "From: support@avocadoandtoast \r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 $message = $this->emailWelcome($confirmationkey);
-$mail = mail($email, 'Please Confirm Project Social Account', $message, $headers);
+$mail = mail($email, 'Please Confirm avocado and  toast Account', $message, $headers);
 return true;
 }
 
@@ -889,7 +889,7 @@ public function emailWelcome($link){
 
                             <h2 style="display: block;margin: 0;padding: 0;color: #202020;font-family: Helvetica;font-size: 24px;font-style: normal;font-weight: bold;line-height: 150%;letter-spacing: normal;text-align: center;">WELCOME *|FNAME|*</h2>
 
-<p style="margin: 10px 0;padding: 0;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #666666;font-family: Georgia, Times, `Times New Roman`, serif;font-size: 16px;line-height: 150%;text-align: center;">Thank you for your joining Project Social. Please confirm your acccount <a href="https://project.social/confirmation?q='.$link.'" class="confirmation-link" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #888888;font-weight: normal;text-decoration: underline;"> HERE. </a><br>
+<p style="margin: 10px 0;padding: 0;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #666666;font-family: Georgia, Times, `Times New Roman`, serif;font-size: 16px;line-height: 150%;text-align: center;">Thank you for your joining Project Social. Please confirm your acccount <a href="https://dev.avocadoandtoast.com/confirmation?q='.$link.'" class="confirmation-link" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #888888;font-weight: normal;text-decoration: underline;"> HERE. </a><br>
 <br>
 I appreciate you taking the time to sign up for&nbsp;our tool that will&nbsp;provide you with realistic pricing for the influencers you want to work with.&nbsp;Here are some additional resources that you can use to get the most out of Project Social!<br>
 &nbsp;</p>
