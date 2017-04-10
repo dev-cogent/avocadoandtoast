3566f9773267ce48e36f468d49ed0a6d6fcd7164
bashirmurtaza93 <bashirmurtaza93@gmail.com>
2017-04-10 18:19:12 -0400

    Deleted files not needed anymore

--- includes/ajax/createlist.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION['userid'])){
-    echo 2; 
-}
-include '../class/list.php';
-$obj = new listoptions;
-$create = $obj->createList($_POST['users'],$_POST['listname'],$_POST['description'],$_SESSION['userid'],$_SESSION['column_id']);
-echo $create;
