8cfbc46b58142771ad054f1bd6f3fc50d052faec
Alejandra Jimenez <axjimenez89@gmail.com>
2017-04-19 15:59:53 -0400

    got rid of left side

--- settings.php
+++ settings.php
@@ -13,6 +13,7 @@
 <link rel="stylesheet" href="/global/fonts/brand-icons/brand-icons.css">
 <link rel="stylesheet" href="/global/fonts/font-awesome/font-awesome.css">
 <link rel="stylesheet" href="/assets/css/discover.css">
+<link rel="stylesheet" href="/assets/css/settings.css">
 <style>
 button:focus{
     box-shadow: 0 0 10px #73C48D;
@@ -30,18 +31,16 @@ a:focus{
 
 <?php include 'php/avocado-nav.php';?>
 
+<div class="col-xs-3"></div>
+<div class="col-xs-6 settings-lg-col" id="setting-container">
 
-
-
-<div class="col-xs-10 settings-lg-col" id="setting-container">
-
-  <div class="input-container" style="width:45%;">
+  <div class="input-container">
 
       <form action="" method="POST" enctype= "multipart/form-data">
 
         <div class="upload-img">
 
-          <div class="uploaded-img-square"><img src="http://avocadoandtoast.com/images/user/<?php echo $userid ;?>.jpg" onerror="this.src=`/assets/images/default-photo.png`" style="height:150px; width:150px;"> </div>
+          <div class="uploaded-img-square"><img id="uploaded-img" src="http://avocadoandtoast.com/images/user/<?php echo $userid ;?>.jpg" onerror="this.src=`/assets/images/default-photo.png`"> </div>
           <div class="profile-title"> Your Avatar </div>
           <input type="file" class="upload-img-btn avocado-hover avocado-focus" name="image" >  Upload Image
         
@@ -49,27 +48,28 @@ a:focus{
 
       <label class="title"> Company Name </label>
       <br/>
-      <input type="text" id="company" name="company" class="form-control category avocado-focus" value="<?php echo $company;?>" maxlength="100" style="">
+      <input type="text" id="company" name="company" class="form-control category avocado-focus" value="<?php echo $company;?>" maxlength="100">
       </input>
       <label class="title"> First Name  </label>
       <br/>
-      <input name="firstname" type="text" class="form-control category avocado-focus" value="<?php echo $firstname;?>"  style="" maxlength="100">
+      <input name="firstname" type="text" class="form-control category avocado-focus" value="<?php echo $firstname;?>"   maxlength="100">
       </input>
 
     <label class="title"> Last Name  </label>
     <br/>
-    <input  name="lastname" type="text" class="form-control category avocado-focus" value="<?php echo $lastname;?>"  style="" maxlength="100">
+    <input  name="lastname" type="text" class="form-control category avocado-focus" value="<?php echo $lastname;?>" maxlength="100">
     </input>
 
       <label class="title"> Email </label>
       <br/>
-      <input name="email" type="email" class="form-control category avocado-focus" value="<?php echo $email;?>"  style="" maxlength="100">
+      <input name="email" type="email" class="form-control category avocado-focus" value="<?php echo $email;?>" maxlength="100">
     </input>
 
       <button class="update-profile-btn col-xs-12"  style="margin-top:30px;" id="submit" type="submit" name="profile"> Update Profile </button>
-       <button class="update-profile-btn col-xs-12"  style="margin-top:30px;" id="getPassword" type="submit" name="profile"> Change Password </button>
+       <button class="update-password-btn col-xs-12"  style="margin-top:30px;" id="getPassword" type="submit" name="profile"> Change Password </button>
       </form>
     </div>
+    <div class="col-xs-3"></div>
   </div>
   </div>
 
