7bd68f19b4db6ce9716db8f3b5a016731d998070
Alejandra Jimenez <axjimenez89@gmail.com>
2017-04-21 16:21:28 -0400

    confirmation-email

---
+++ confirm-email.php
@@ -0,0 +1,15 @@
+
+
+<div class="thank-you-email">
+    <p class="confirm-email"> 
+        Done!
+        <br>
+        An email has been sent to the address you provided.
+        <br>
+         Please check your email
+        and follow the instructions. 
+        <div class="skating-avo"> 
+        <img src="images/shred_2.gif" alt="email-confirmation"/>
+
+        <button 
+</div>
