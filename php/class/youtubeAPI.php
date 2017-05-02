5de351365f9c85fdfd17b65b44d35c6383b2755d
bashirmurtaza93 <bashirmurtaza93@gmail.com>
2017-05-02 15:04:47 -0400

    Fixed issues with the youtube API class

--- php/class/youtubeAPI.php
+++ php/class/youtubeAPI.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 
 class youtubeAPI{
 
-    protected $youtubeKey = 'AIzaSyAAANUEzxJ9RkLAZOoVxxgP5hrxmrzUOnc';
+    private $youtubeKey = 'AIzaSyAAANUEzxJ9RkLAZOoVxxgP5hrxmrzUOnc';
 
 
     protected function getYoutubeAPIKey(){
@@ -41,7 +41,6 @@ class youtubeAPI{
 
     public function searchInfluencer($user){
         $key = $this->getYoutubeAPIKey();
-        
     }
 
 
@@ -52,15 +51,13 @@ class youtubeAPI{
         $key = $this->getYoutubeAPIKey();
         $url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId='.$channelID.'&maxResults=12&order=date&key='.$key;
         $apiData = $this->curl($url);
-        print_r($apiData);
         foreach ($apiData['items'] as $info){
             $videoID = $info['id']['videoId'];
             array_push($videoArr,$videoID);
         }
         //PLEASE REMOVE
-        $this->getVideoEngagement($videoArr);
-        return $videoArr;
-        
+        //$this->getVideoEngagement($videoArr);
+        $this->videos = $videoArr;
     }
 
 
@@ -79,34 +76,28 @@ class youtubeAPI{
             $engagementObj->$videoID->comments = $info['statistics']['commentCount'];
             $engagementObj->$videoID->total_engagement = $engagementObj->$videoID->likes  + $engagementObj->$videoID->comments;
             $engagementObj->$videoID->avg_engagement = $engagementObj->$videoID->total_engagement/(count($videoArr));
-            
+
         }
         print_r($engagementObj);
     }
 
 
-    
+
 
     public function curl($url) {
         $curl_connection = curl_init($url);
         curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 30);
         curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl_connection, CURLOPT_SSL_VERIFYPEER, false);
-        $json = json_decode(curl_exec($curl_connection), true); 
+        $json = json_decode(curl_exec($curl_connection), true);
         curl_close($curl_connection);
-        return $json;     
-        
-    } // end curl 
+        return $json;
+
+    } // end curl
 
 
 }
-//test youtube channel 
+//test youtube channel
 // $youtube = new youtubeAPI();
 // $meh = $youtube->getYoutubeVideos('UCYEK6xds6eo-3tr4xRdflmQ');
 //$meh = $youtube->getChannelTopicDetails('biolayne');
-
-
-
-
-
-
