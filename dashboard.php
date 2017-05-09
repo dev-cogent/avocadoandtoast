af9e426d704c4d290f20ce2094d0dfbc5a40d6bc
Gaby <gabyhernandezlebron@gmail.com>
2017-05-09 12:14:55 -0400

    are you sure you want to delete pop up

--- dashboard.php
+++ dashboard.php
@@ -15,17 +15,21 @@
 
 <?php include 'php/avocado-nav.php'; ?>
 <body class="col-xs-12" style="padding-left:0px;padding-right:0px;">
-    
 
 
- 
+  <div class="bootbox-body">
+  <div class="delete-icon-popup-div"> <img src="/assets/images/delete.png" class="trash-icon"/> </div>
+  <div class="row"> <div class="delete-popup-detail">   <span class="delete-list-text"> DELETE LIST?  </span> <br/> Once deleted this action cannot be undone </div>
+  <div class="delete-btn-div"> <button class="delete-yes primary-button" type="button" formaction="/campaigns/?id=' + campaignid + '"> DELETE </button> <button class="delete-no secondary-button" type="button" formaction="/campaigns/?id=' + campaignid + '"> NOPE </button></div>
+  </div>
+
 
     <!-- Start Campains here -->
 
 
     <div id="campaign-container">
          <!-- All campaigns go in here -->
-         <!-- Information here is appened in javascript --> 
+         <!-- Information here is appened in javascript -->
     </div>
 
     <?php include 'footer.php'; ?>
@@ -33,5 +37,3 @@
 </body>
 
 <script src="/assets/js/avocado-dashboard.js"></script>
-
-
