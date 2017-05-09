6a029959ce8636ce36acd6fb0e822e8228767bd0
Bashir Murtaza <bashirmurtaza93@gmail.com>
2017-05-09 12:15:14 -0400

    Fixed some bugs with new sidebar Added a better autofill feature Feature is a bit hidden for the time being, it needs some styling If you press tab while typing for now it may autofill your correct search

--- php/ajax/avocado-discover-get-filter-results.php
+++ php/ajax/avocado-discover-get-filter-results.php
@@ -10,12 +10,14 @@ $bio = $filters['keywords'];
 $options = $filters['options'];
 $searchoptions = $filters['search'];
 $searchuser = $filters['user'];
+$gender = $filters['gender'];
+$location = $filters['location'];
 
 //checking if the user is filtering for followers and / or engagement.
-$engmin = $filters['eng-min'];
-$engmax = $filters['eng-max'];
-$max = intval($filters['max']);
-$min = intval($filters['min']);
+$engmin = $filters['engagement']['min'];
+$engmax = $filters['engagement']['max'];
+$max = intval($filters['followers']['max']);
+$min = intval($filters['followers']['min']);
 $platforms = $filters['platform'];
 $position = $_POST['page'] * 24;
 $users = array();
@@ -23,14 +25,17 @@ $where = "";
 $rownum = 0;
 $arr = array();
 
+
 if($bio !== NULL) $temparr = checkBio($bio, $searchoptions, $options, $where, $arr);
-// if($searchuser !== NULL) $temparr = checkUser($searchuser, $where, $arr);
-if($platforms !== NULL) $temparr =checkPlatform($min,$max,$engmin,$engmax, $where, $arr,$platforms);
+if($max !== NULL) $temparr =checkPlatform($min,$max,$engmin,$engmax, $where, $arr,$platforms);
+if($location) $temparr =checkLocation($location, $where, $arr);
+if($gender) $temparr = checkGender($gender, $where, $arr);
+
+
 $binding = array();
 $params = $arr['term'];
 if($position < 0) $position = 0;
 unset($stmt);
-
 $stmt = $conn->prepare("SELECT COUNT(*) FROM `Influencer_Information` $where");
 if($where != ''){
 $types = '';
@@ -59,8 +64,6 @@ while($stmt->fetch()){
 
 $jsonarr = json_encode($jsonarr);
 echo $jsonarr;
-
-
 
 
 
@@ -104,35 +107,74 @@ function checkBio($bio, $searchoptions, $options, &$where, &$arr){
 }
 
 
+function checkGender($gender, &$where, &$arr){
 
-function checkPlatform($mincount,$maxcount,$mineng,$maxeng, &$where, &$arr,$platforms){
-    $i = 1;
-    foreach($platforms as $platform){
-
-        $query = '`'.$platform.'_count` >= ? AND `'.$platform.'_count` <= ?) AND (`'.$platform.'_eng` >= ? AND `'.$platform.'_eng` <= ?';
-        $arr['term'][] = $mincount;
-        $arr['term'][] = $maxcount;
-        $arr['term'][] = $mineng;
-        $arr['term'][] = $maxeng;
-
-       if(checkWhere($where)){
-          if($i == 1){
-            $where .= "AND (($query) ";
-            $i = 0;
-          }else{
-            $where .= "AND ($query) ";
-          }
-       }else{
-          $where .= "WHERE (($query) ";
-          $i = 0;
-       }
-
-    }
-
-    $where = $where.')';
-    return $arr;
+        if(count($gender) == 2 || count($gender) == 0){
+          return 0;
+        }
+         $gender = $gender[0];
+         $query = '`gender` LIKE ?';
+         $arr['term'][] = $gender;
+         $where .= "AND ($query) ";
+         return $arr;
 }
 
+
+function checkLocation($location, &$where, &$arr){
+         $query = '`location` LIKE ?';
+         $arr['term'][] = '%'.$location.'%';
+         $where .= "AND ($query) ";
+         return $arr;
+}
+
+
+
+function checkPlatform($mincount,$maxcount,$mineng,$maxeng, &$where, &$arr,$platforms){
+    if(count($platforms) == 0){
+      $query = '`total` >= ? AND `total` <= ?';
+      $arr['term'][] = $mincount;
+      $arr['term'][] = $maxcount;
+      if(checkWhere($where)){
+         if($i == 1){
+           $where .= "AND (($query) ";
+           $i = 0;
+         }else{
+           $where .= "AND ($query ";
+         }
+      }else{
+         $where .= "WHERE (($query) ";
+         $i = 0;
+      }
+
+      $where = $where.')';
+    }
+    else{
+      foreach($platforms as $platform){
+
+          $query = '`'.$platform.'_count` >= ? AND `'.$platform.'_count` <= ?) AND (`'.$platform.'_eng` >= ? AND `'.$platform.'_eng` <= ?)';
+          $arr['term'][] = $mincount;
+          $arr['term'][] = $maxcount;
+          $arr['term'][] = (float)$mineng;
+          $arr['term'][] = (float)$maxeng;
+          if(checkWhere($where)){
+            if($i == 1){
+              $where .= "AND (($query) ";
+              $i = 0;
+            }else{
+              $where .= "AND (($query ";
+            }
+          }else{
+            $where .= "WHERE (($query ";
+            $i = 0;
+          }
+
+          $where = $where.')';
+      }
+  }
+
+
+    return $arr;
+}
 
 
 
